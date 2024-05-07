0066c6aa06e0a7e44904f90f7dc93131d844a596
Stefan Link <mail@stefanlink.info>
2024-05-07 07:38:22 +0200

    Lighthouse Findings

--- src/components/IconTitle.tsx
+++ src/components/IconTitle.tsx
@@ -13,7 +13,7 @@ const IconTitle: React.FC<IconTitleProps> = ({ titleKey }) => {
   return (
     <div className="title-with-icon">
       <picture className="title-icon">
-        <source srcSet="icon_small.webp" type="image/webp" />
+        <source srcSet="/misc/icon_small.webp" type="image/webp" />
         <img src="icon_small.png" width="32" height="32" alt={t('Header.IconTitle')} className="title-icon" />
       </picture>
       <div className="byrd-xl-extra-bold">
